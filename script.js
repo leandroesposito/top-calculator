5235db5c4de974611736a6e21e96b185ca61cf8c
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-01-11 17:11:05 -0300

    Create main functionality elements

---
+++ script.js
@@ -0,0 +1,47 @@
+const operations = {
+    add(a, b) {
+        return a + b;
+    },
+    substract(a, b) {
+        return a - b;
+    },
+    multiply(a, b) {
+        return a * b;
+    },
+    divide(a, b) {
+        if (b == 0) {
+            return "CAN'T DIVIDE BY ZERO";
+        }
+
+        return a / b;
+    }
+};
+
+const calculation = {
+    num1: null,
+    num2: null,
+    operand: null,
+}
+
+function operate(num1, num2, operator) {
+    let result = null;
+    switch (operator) {
+        case "+":
+            result = operations.add(num1, num2);
+            break;
+        case "-":
+            result = operations.substract(num1, num2);
+            break;
+        case "*":
+            result = operations.multiply(num1, num2);
+            break;
+        case "/":
+            result = operations.divide(num1, num2);
+            break;
+        default:
+            result = "OPERATION NOT SUPPORTED";
+            break;
+    }
+
+    return result;
+}
