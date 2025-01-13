a3de200955c0d1abb0ac83b07b639ad2c9e7ae9a
Leandro Esposito <71948697+leandroesposito@users.noreply.github.com>
2025-01-12 21:30:51 -0300

    Add decimal calculation functionality

--- script.js
+++ script.js
@@ -24,29 +24,37 @@ const operations = {
 const calculation = {
     num1: null,
     num2: null,
+    decimals: "",
     operator: null,
+    decimalMode: false,
     clearMemory() {
         this.num1 = null;
         this.num2 = null;
+        this.decimals = "";
         this.operator = null;
+        this.decimalMode = false;
     },
     setOperator(operator) {
         this.operator = operator;
         if (this.num1 === null && this.num2 !== null) {
             this.switchNumbers();
         }
+        this.decimals = "";
     },
     switchNumbers() {
-        this.num1 = this.num2;
+        this.num1 = this.getNum2();
         this.num2 = null;
+        this.decimalMode = false;
     },
     calculate() {
         let result = null;
         if (this.allSet()) {
-            result = operate(this.num1, this.num2, this.operator);
+            result = operate(this.getNum1(), this.getNum2(), this.operator);
             this.clearMemory();
             if (!isNaN(result)) {
-                this.num2 = result;
+                let [digits, decimals] = result.toString().split(".")
+                this.num2 = Number(digits);
+                this.decimals = decimals;
             }
         }
         return result;
@@ -55,6 +63,32 @@ const calculation = {
         return (this.num1 !== null &&
             this.num2 !== null &&
             this.operator !== null);
+    },
+    decimalModeOn() {
+        this.decimalMode = true;
+        this.num2 = this.num2 ?? 0;
+    },
+    appendNumber(number) {
+        if (this.decimalMode) {
+            this.decimals += number.toString();
+        }
+        else {
+            this.num2 = this.num2 * 10 + number;
+        }
+    },
+    getNum1() {
+        return this.num1;
+    },
+    getNum2() {
+        if (this.decimals) {
+            return parseFloat(this.num2ToString());
+        }
+        else {
+            return this.num2;
+        }
+    },
+    num2ToString() {
+        return this.num2.toString() + (this.decimals ? "." + this.decimals : "");
     }
 }
 
@@ -88,19 +122,27 @@ numbersKeypad.addEventListener("click", (event) => {
             calculation.clearMemory();
         }
         const buttonValue = getDataValue(target);
-        addDigit(buttonValue);
+        appendNumber(buttonValue);
     }
     else if (target.classList.contains("clear")) {
         calculation.clearMemory();
         clearDisplay();
     }
+    else if (target.classList.contains("dot")) {
+        if (calculation.operator === "=") {
+            calculation.clearMemory();
+        }
+        if (!calculation.decimalMode) {
+            calculation.decimalModeOn();
+            updateDisplay(calculation.num2ToString());
+        }
+    }
 });
 
-function addDigit(number) {
+function appendNumber(number) {
     newDigit = +number;
-    calculation.num2 = calculation.num2 * 10 + newDigit;
-
-    updateDisplay(calculation.num2);
+    calculation.appendNumber(newDigit);
+    updateDisplay(calculation.num2ToString());
 }
 
 function clearDisplay() {
